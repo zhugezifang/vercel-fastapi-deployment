bdd633239e2983343841d29e8ab6e44a7f2219d4
诸葛子房 <xiaoqiu2017wy@163.com>
2024-03-25 15:14:20 +0800

    feat:templates

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ app.add_middleware(
 )
 
 templates = Jinja2Templates(directory="templates")
-@app.get("/hello/", response_class=HTMLResponse)
-async def hello(request: Request):
-   return templates.TemplateResponse("hello.html", {"request": request})
+
+@app.get("/hello/{name}", response_class=HTMLResponse)
+async def hello(request: Request, name:str):
+   return templates.TemplateResponse("hello.html", {"request": request, "name":name})
 
 html = f"""
 <!DOCTYPE html>
